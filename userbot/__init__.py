fcfb146bb876b03b860856ee7fab9e002125c210
KENZO-420 <78176892+KENZO-420@users.noreply.github.com>
2021-03-09 11:19:24 +0000

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -349,7 +349,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} ✞".format("✞", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} ☠".format("☠", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -365,10 +365,10 @@ def paginate_help(page_number, loaded_modules, prefix):
         ] + [
             (
                 custom.Button.inline(
-                    "⛧", data="{}_prev({})".format(prefix, modulo_page)
+                    "◀ᴘʀᴇᴠ", data="{}_prev({})".format(prefix, modulo_page)
                 ),
                 custom.Button.inline(
-                    "⛧", data="{}_next({})".format(prefix, modulo_page)
+                    "ɴᴇxᴛ▶", data="{}_next({})".format(prefix, modulo_page)
                 )
             )
         ]
@@ -403,8 +403,8 @@ with bot:
                 buttons = paginate_help(0, dugmeler, "helpme")
                 result = builder.article(
                     "Harap Gunakan .help Untuk Perintah",
-                    text="{}\n\n**➸ Jumlah Modul Yang Tersedia:** `{}`\n               \n**➸ Daftar Modul 404-Userbot:** \n".format(
-                        "**⛧ ╔404-Userbot╗ ⛧**",
+                    text="{}\n\n**✇⟩ Jumlah Modul Yang Tersedia:** `{}`\n               \n**✇⟩ Daftar Modul 404-Userbot:** \n".format(
+                        "**⛧ ╔╡404-Userbot╞╗ ⛧**",
                         len(dugmeler),
                     ),
                     buttons=buttons,
@@ -412,13 +412,13 @@ with bot:
                 )
             elif query.startswith("tb_btn"):
                 result = builder.article(
-                    "Bantuan ⛧ ╔404-Userbot╗ ⛧",
+                    "Bantuan *⛧ ╔╡404-Userbot╞╗ ⛧",
                     text="Daftar Modul",
                     buttons=[],
                     link_preview=True)
             else:
                 result = builder.article(
-                    "**⛧ ╔404-Userbot╗ ⛧**",
+                    "***⛧ ╔╡404-Userbot╞╗ ⛧**",
                     text="""**Anda Bisa Membuat 404-Userbot Anda Sendiri Dengan Cara:** [Tekan Disini](t.me/Lorduserbot_Group)""",
                     buttons=[
                         [
