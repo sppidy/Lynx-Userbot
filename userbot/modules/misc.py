6f0ac2843f6f7317f9d3fd2e5864dc76ac11e114
KENZO-420 <78176892+KENZO-420@users.noreply.github.com>
2021-03-12 02:45:38 +0000

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "❃ **Repo :** [404-Userbot](https://github.com/KENZO-420/404-Userbot)\n❃ **Owner :** [Axel](t.me/@SyndicateTwenty4)"
+        "❃ **Repo :** [404-Userbot](https://github.com/KENZO-420/404-Userbot)\n❃ **Owner :** [Axel](t.me/SyndicateTwenty4)"
     )
 
 
