06abe873c4a9773a768f2b96a92aca4c58eed21f
KENZO-420 <78176892+KENZO-420@users.noreply.github.com>
2021-03-13 06:03:38 +0000

    Update oi.py

--- userbot/modules/oi.py
+++ userbot/modules/oi.py
@@ -2,15 +2,15 @@ from time import sleep
 from userbot.events import register
 
 
-@register(outgoing=True, pattern='^.alvin(?: |$)(.*)')
+@register(outgoing=True, pattern='^.axel(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(3)
-    await typew.edit("`Hai Perkenalkan Namaku Alvin`")
+    await typew.edit("`Hai Perkenalkan Namaku Axel`")
     sleep(3)
-    await typew.edit("`17 Tahun`")
+    await typew.edit("`21 Tahun`")
     sleep(1)
-    await typew.edit("`Tinggal Dibatam, Salam Kenal:)`")
+    await typew.edit("`Tinggal Di Tangerang, Salam Kenal:)`")
 # Create by myself @localheart
 
 
