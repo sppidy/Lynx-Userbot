d164f118d54d013a42ebe78d02a3a6520a0c36ac
KENZO-420 <78176892+KENZO-420@users.noreply.github.com>
2021-03-13 10:31:13 +0000

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -98,7 +98,7 @@ async def type_afk_is_not_true(notafk):
     afk_end = back_alive.replace(microsecond=0)
     if ISAFK:
         ISAFK = False
-        msg = await notafk.respond("**`{ALIVE_NAME}` ᴛᴇʟᴀʜ ᴋᴇᴍʙᴀʟɪ !!!**")
+        msg = await notafk.respond("** ᴛᴇʟᴀʜ ᴋᴇᴍʙᴀʟɪ !!!**")
         time.sleep(3)
         await msg.delete()
         await notafk.client(UpdateProfileRequest(first_name=user.first_name, last_name=last1))
