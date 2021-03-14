41d9a17f3b84c32e4dfea39ef11ecf7bdf0a7400
KENZO-420 <78176892+KENZO-420@users.noreply.github.com>
2021-03-14 01:27:33 +0000

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -355,8 +355,8 @@ CMD_HELP.update({
 \n↳ : __Shows the userbot version.__\
 \n\n⚡**CMD**⚡: `.pip` <module(s)>\
 \n↳ : __Does a search of pip modules(s).__\
-\n\n⚡**CMD*"⚡: `.notfound`\
-\n↳ : __Type .start to see whether your bot is working or not.__\
+\n\n⚡**CMD**"⚡: `.notfound`\
+\n↳ : __Type .notfound to see whether your bot is working or not.__\
 \n\n⚡**CMD**⚡: `.aliveu` <text>\
 \n↳ : __Changes the 'user' in alive to the text you want.__\
 \n\n⚡**CMD**⚡: `.resetalive`\
