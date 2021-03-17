8f03a733ccef174c891759aa0e26e0bea8875e0d
KENZO-404 <80487896+KENZO-404@users.noreply.github.com>
2021-03-17 08:10:12 +0000

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -297,6 +297,7 @@ async def amireallyalive(alive):
 async def amireallyalive(alive):
     user = await bot.get_me()
     await get_readable_time((time.time() - StartTime))
+    start = datetime.now()
     await alive.edit("__C__")
     await alive.edit("__Co__")
     await alive.edit("__Con__")
@@ -320,6 +321,7 @@ async def amireallyalive(alive):
     await alive.edit("__Connecting to server...__")
     sleep(1)
     await alive.edit("⚡𝗟𝘆𝗻𝘅-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡ __Aktif...__")
+    end = datetime.now()
     output = (
         f"**ㅤㅤ  ╭─━━━━━━━━━━━─╮** \n"
         f"**       ⊏┊⚡𝗟𝘆𝗻𝘅-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡ ⊨〛💨 ** \n"
