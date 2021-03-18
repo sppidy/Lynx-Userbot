89d112a929358e91281b18b222b1a88bca599b07
KENZO-404 <80487896+KENZO-404@users.noreply.github.com>
2021-03-18 17:37:58 +0000

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -219,7 +219,7 @@ async def pipcheck(pip):
         await pip.edit("Gunakan `.help pip` Untuk Melihat Contoh")
 
 
-@register(outgoing=True, pattern=r"^\.(?:lynx|lynxon)\s?(.)?")
+@register(outgoing=True, pattern=r"^\.(?:Lynx|Lynxon)\s?(.)?")
 async def amireallyalive(alive):
     user = await bot.get_me()
     await get_readable_time((time.time() - StartTime))
