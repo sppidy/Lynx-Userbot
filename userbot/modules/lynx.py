9652e2b4ff3e961a5e691bb26d7b2c02ab62cae2
KENZO-404 <80487896+KENZO-404@users.noreply.github.com>
2021-03-21 18:46:30 +0000

    Update lynx.py

--- userbot/modules/lynx.py
+++ userbot/modules/lynx.py
@@ -51,7 +51,7 @@ async def typewriter(typew):
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("╭╮╱╱╭╮"
-                     "┃╰╮╭╯┃
+                     "┃╰╮╭╯┃"
                      "╰╮╰╯╭┻━┳╮╭╮"
                      "╱╰╮╭┫╭╮┃┃┃┃"
                      "╱╱┃┃┃╰╯┃╰╯┃"
