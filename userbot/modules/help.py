71cc7ba492c07c0dbfc1472d6d2c26152c35e02b
KENZO-404 <80487896+KENZO-404@users.noreply.github.com>
2021-03-27 20:19:09 +0000

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -42,7 +42,7 @@ async def help(event):
             string += "`\t|  "
         await event.edit("**⚡𝗟𝘆𝗻𝘅-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡**\n**Mᴀɪɴ Mᴇɴᴜ :**/n"
                          f"╰►|{string} ◄─\n"
-                         f"◑» Bᴏᴛ ᴏꜰ {DEFAULTUSER}/n◑» Mᴏᴅᴜʟᴇꜱ : `(len{modules}` ")
+                         f"◑» Bᴏᴛ ᴏꜰ {DEFAULTUSER}/n◑» Mᴏᴅᴜʟᴇꜱ : {len(modules)} ")
         await event.reply(f"\n**Contoh** : Ketik `.help afk` Untuk Informasi Pengunaan./nAtau Bisa Juga Ketik `.helpme` Untuk Main Menu Yang Lain-Nya.")
         await asyncio.sleep(1000)
         await event.delete()
