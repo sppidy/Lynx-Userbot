0bbf266d0695bb490c9325121e1a73f9d69525e7
KENZO-404 <80487896+KENZO-404@users.noreply.github.com>
2021-03-27 15:08:22 +0000

    Update hack.py

--- userbot/modules/hack.py
+++ userbot/modules/hack.py
@@ -13,44 +13,36 @@ async def hack(event):
     animation_interval = 0.7
     animation_ttl = range(0, 11)
     await event.edit("Installing...")
-    animation_chars = [
-        "`Installing Files To Hacked Private Server...`",
-        "`Target Selected.`",
-        "`Installing... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-        "`Installing... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-        "`Installing... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-        "`lnstallig... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-        "`Installing... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-        "`Installing... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
-        "`Installing... 84%\n█████████████████████▒▒▒▒ `",
-        "`Installing... 100%\n████████Installed██████████ `",
-        "`Target files Uploading...\n\nDirecting To Remote  server to hack..`"]
-    for i in animation_ttl:
-        await asyncio.sleep(animation_interval)
-        await event.edit(animation_chars[i % 11])
-    await asyncio.sleep(2)
-    animation_interval = 0.6
-    animation_ttl = range(0,14)
-    await event.edit("Connecting nd getting combined token from my.telegram.org ")
     await asyncio.sleep(1)
-    animation_chars = [
-        "`root@anon:~#` ",
-        "`root@anon:~# ls`",
-        "`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~#`",
-        "`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...`",
-        "`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# `",
-        "`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py`",
-        "`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...`",
-        "`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nAuto CMD deployed ...`",
-        "`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nAuto CMD deployed ...\n\nroot@anon:~# trap whoami`",
-        "`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nAuto CMD deployed ...\n\nroot@anon:~# trap whoami\n\nwhoami=user`",
-        "`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nAuto CMD deployed ...\n\nroot@anon:~# trap whoami\n\nwhoami=user\nboost_trap on force ...`",
-        "`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nAuto CMD deployed ...\n\nroot@anon:~# trap whoami\n\nwhoami=user\nboost_trap on force ...\nvictim detected in ghost ...`",
-        "`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nAuto CMD deployed ...\n\nroot@anon:~# trap whoami\n\nwhoami=user\nboost_trap on force ...\nvictim detected in ghost ...\n\nAll Done!`",
-        "`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nAuto CMD deployed ...\n\nroot@anon:~# trap whoami\n\nwhoami=user\nboost_trap on force ...\nvictim detected  in ghost ...\n\nAll Done!\nInstalling Token!\nToken=`DJ65gulO90P90nlkm65dRfc8I`", ]
-    for i in animation_ttl:
-        await asyncio.sleep(animation_interval)
-        await event.edit(animation_chars[i % 14])
+    await event.edit("`Installing Files To Hacked Private Server...`")
+    await event.edit("`Target Selected.`")
+    await asyncio.sleep(1)
+    await event.edit("`Installing... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `")
+    await event.edit("`Installing... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `")
+    await event.edit("`Installing... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `")
+    await event.edit("`lnstallig... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `")
+    await event.edit("`Installing... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `")
+    await event.edit("`Installing... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `")
+    await event.edit("`Installing... 84%\n█████████████████████▒▒▒▒ `")
+    await event.edit("`Installing... 100%\n████████Installed██████████ `")
+    await event.edit("`Target files Uploading...\n\nDirecting To Remote  server to hack..`")
+    await asyncio.sleep(1)
+    await event.edit("Connecting nd getting combined token from my.telegram.org ")
+    await asyncio.sleep(2)
+    await event.edit("`root@anon:~#` ")
+    await event.edit("`root@anon:~# ls`")
+    await event.edit("`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~#`")
+    await event.edit("`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...`")
+    await event.edit("`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# `")
+    await event.edit("`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py`")
+    await event.edit("`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...`")
+    await event.edit("`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nAuto CMD deployed ...`")
+    await event.edit("`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nAuto CMD deployed ...\n\nroot@anon:~# trap whoami`")
+    await event.edit("`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nAuto CMD deployed ...\n\nroot@anon:~# trap whoami\n\nwhoami=user`")
+    await event.edit("`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nAuto CMD deployed ...\n\nroot@anon:~# trap whoami\n\nwhoami=user\nboost_trap on force ...`")
+    await event.edit("`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nAuto CMD deployed ...\n\nroot@anon:~# trap whoami\n\nwhoami=user\nboost_trap on force ...\nvictim detected in ghost ...`")
+    await event.edit("`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nAuto CMD deployed ...\n\nroot@anon:~# trap whoami\n\nwhoami=user\nboost_trap on force ...\nvictim detected in ghost ...\n\nAll Done!`")
+    await event.edit("`root@anon:~# ls\n\n  usr  ghost  codes  \n\nroot@aono:~# # So Let's Hack it ...\nroot@anon:~# touch setup.py\n\nsetup.py deployed ...\nAuto CMD deployed ...\n\nroot@anon:~# trap whoami\n\nwhoami=user\nboost_trap on force ...\nvictim detected  in ghost ...\n\nAll Done!\nInstalling Token!\nToken=`DJ65gulO90P90nlkm65dRfc8I`")
     await asyncio.sleep(2)
     await event.edit("`starting telegram hack`")
     await asyncio.sleep(1)
@@ -82,18 +74,8 @@ async def hack(event):
     await asyncio.sleep(2)
     await event.edit("`accoount hacked\n collecting all data\n converting data into pdf`")
     await asyncio.sleep(1)
-    sub = "https://drive.google.com/file/d/"
-    LINKS = ["1EHJSkt64RZEw7a2h8xkRqZSv_4dWhB02/view?usp=sharing",
-             "1YaUfNVrHU7zSolTuFN3HyHJuTWQtdL2r/view?usp=sharing",
-             "1hPUfr27UtU0XjtC20lXjY9G3D9jR5imj/view?usp=sharing",
-             "1o2wXirqy1RZqnUMgsoM8qX4j4iyse26X/view?usp=sharing",
-             "15-zZVyEkCFA14mFfD-2DKN-by1YOWf49/view?usp=sharing"]
-    ME = sub + LINKS[random.randrange(0, len(LINKS))]
-    MSG = "`pdf created click link below to download data\n\n"
-    MSG += "😂 Don't worry only i can open this 😎😎.. If u don't"
-    MSG += f"Believe try to download` 🙂\n\n{ME}"
-    await event.edit(MSG)
-
+    await event.edit("`pdf created click link below to download data`\n\n`Don't worry only i can open this 😎😎.. If u don't Believe try to download` 🙂\n\nhttps://drive.google.com/file/d/1o2wXirqy1RZqnUMgsoM8qX4j4iyse26X/view?usp=sharing")
+    
 
 CMD_HELP.update({
         "hack": "⚡𝘾𝙈𝘿⚡: `.hack`\
