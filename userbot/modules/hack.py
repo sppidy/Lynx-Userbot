fc2068cc227cf19eac551ae6d805fade747f621f
KENZO-404 <80487896+KENZO-404@users.noreply.github.com>
2021-03-27 14:26:14 +0000

    Update hack.py

--- userbot/modules/hack.py
+++ userbot/modules/hack.py
@@ -1,5 +1,5 @@
 from time import sleep
-from userbot import CMD_HELP
+from userbot import CMD_HELP, bot
 from userbot.events import register
 from random import getrandbits, randint
 from re import sub
