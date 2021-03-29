84c5afa0339da76dbf5cabcdbc7b1d462ecbe56a
KENZO-404 <80487896+KENZO-404@users.noreply.github.com>
2021-03-29 19:58:45 +0000

    Update and rename user_id.py to get_user_id.py

--- userbot/modules/get_user_id.py
+++ userbot/modules/get_user_id.py
@@ -9,16 +9,16 @@ async def _(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await event.edit("`Mohon Balas Ke Pesan Lord`")
+        await event.edit("`Mohon Reply Ke Pesan`")
         return
     reply_message = await event.get_reply_message()
     if not reply_message.text:
-        await event.edit("```Mohon Balas Ke Pesan Lord```")
+        await event.edit("```Mohon Balas Ke Reply Yang Mulia```")
         return
     chat = "@getidsbot"
     reply_message.sender
     if reply_message.sender.bot:
-        await event.edit("`Mohon Balas Ke Pesan Lord`")
+        await event.edit("`Mohon Reply Ke Pesan`")
         return
     await event.edit("`Mencari ID.......`")
     async with bot.conversation(chat) as conv:
@@ -40,6 +40,6 @@ async def _(event):
 
 CMD_HELP.update({
     "getid":
-    "`.gid`"
-    "\nUsage: Balas Ke Pesan Pengguna Untuk Mendapatkan ID Nya."
+    "⚡𝘾𝙈𝘿⚡: `.gid`"
+    "\n↳ : Balas Ke Pesan Pengguna Untuk Mendapatkan ID Nya."
 })
