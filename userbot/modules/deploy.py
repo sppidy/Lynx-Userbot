30101aadc2bfa22fc6fa36bd077d13213564ced5
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-04-05 22:11:34 +0000

    Update deploy.py

--- userbot/modules/deploy.py
+++ userbot/modules/deploy.py
@@ -26,47 +26,36 @@ async def _(event):
 
     animation_interval = 10
 
-    animation_ttl = range(0, 12)
+    animation_ttl = range(0, 22)
 
-   # input_str = event.pattern_match.group(1)
+    input_str = event.pattern_match.group(1)
 
+    if input_str == "deploy":
 
-
-    await event.edit("Deploying.")
-    await event.edit("Deploying..")
-    await event.edit("Deploying...")
-    await event.edit("Deploying.")
-    await event.edit("Deploying..")
-    await event.edit("Deploying...")
-    await event.edit("Deploying.")
-    await event.edit("Deploying..")
-    await event.edit("Deploying...")
-    await event.edit("Deploying.")
-    await event.edit("Deploying..")
-    await event.edit("Deploying...")
+        await event.edit(input_str)
 
     animation_chars = [
         
-            "**Heroku Connecting To Latest Github Build (KENZO-404/Lynx-Userbot)**",
-            "**Build started by user** **{DEFAULTUSER}**",
-            "**Deploy** `535a74f0` **by user** **{DEFAULTUSER}**",
-            "**Restarting Heroku Server...**",
-            "**State changed from up to starting**",    
-            "**Stopping all processes with SIGTERM**",
-            "**Process exited with** `status 143`",
-            "**Starting process with command** `python3 -m userbot`",
-            "**State changed from starting to up**",
-            "telethon.network.mtprotosender - INFO - Connecting to 91.108.56.170:443/TcpFull...",
-            "telethon.network.mtprotosender - INFO - Connection to 91.108.56.170:443/TcpFull complete!",
-            "telethon.network.mtprotosender - INFO - Disconnection from 91.108.56.146:443/TcpFull complete!",
-            "telethon.network.mtprotosender - INFO - Disconnecting from 91.108.56.146:443/TcpFull...",
-            "userbot - INFO - Modules to load: ['__help', 'admin', 'adzan', 'afk', 'allscrapers', 'android', 'anilist', 'animasi', 'anime', 'anti_spambot', 'antiflood', 'aria', 'ascii', 'bitly', 'blacklist', 'carbon', 'chat', 'coolprofilepics', 'covid', 'create', 'createstickers', 'dbs', 'deepfry', 'deezloader', 'detection', 'emojigames', 'eval', 'fakegban', 'federasi', 'figlet', 'filemanager', 'filter', 'games', 'gban', 'gcast', 'gdrive', 'get_user_id', 'getmusic', 'gid', 'gitcommit', 'github', 'glitcher', 'globalban', 'googlephotos', 'gps', 'hack', 'hash', 'help', 'hentai', 'herokuapp', 'igsaver', 'imgmemes', 'imp', 'kekuatan', 'kenzo', 'kenzogans', 'kenzogans24', 'lastfm', 'lock', 'lynxae', 'lynxhelper', 'lynxhz', 'lynxmemes', 'lynxmisc', 'lynxtt', 'lynxwc', 'lyrics', 'mega_downloads', 'memes', 'memify', 'mentions', 'messages', 'misc', 'nekobot', 'notes', 'offline', 'oi', 'phreaker', 'pms', 'profile', 'quotly', 'rastick', 'resi', 'reverse', 'salam', 'sangmata', 'santet', 'sed', 'snips', 'spam', 'spotifynow', 'ss_video', 'statme', 'stext', 'stickers', 'stickers_v2', 'system_stats', 'tag_all', 'telegraph', 'tempmail', 'time_date', 'tiny', 'torrentsearch', 'transform', 'updater', 'upload_download', 'waifu','transform', 'updater', 'upload_download', 'waifu', 'wallpaper', 'weather', 'webupload', 'welcomes', 'whois', 'www', 'xiaomi', 'zipfile']", 
-            "telethon.network.mtprotosender - INFO - Connecting to 91.108.56.146:443/TcpFull...",
-            "telethon.network.mtprotosender - INFO - Connection to 91.108.56.146:443/TcpFull complete!",
-            "telethon.network.mtprotosender - INFO - Received response without parent request",
-            "INFO: Lynx-Userbot:Logged in as 557667062`",
-            "INFO: Lynx-Userbot:Successfully loaded all plugins",
-            "2021-04-05T16:47:20.919852+00:00 app[worker.1]: 2021-04-05 16:47:20,919 - userbot -",
+            "Heroku Connecting To Latest Github Build (KENZO-404/Lynx-Userbot)",
+            f"Build started by user `{DEFAULTUSER}`",
+            f"Deploy `535a74f0` by user `{DEFAULTUSER}`",
+            "`Restarting Heroku Server...`",
+            "State changed from up to starting",    
+            "Stopping all processes with **SIGTERM**",
+            "Process exited with `status 143`",
+            "Starting process with command\n`[python3 -m userbot]`",
+            "State changed from starting to up",
+            "telethon.network.mtprotosender -\nINFO - Connecting to 91.108.56.170:443/TcpFull...",
+            "telethon.network.mtprotosender -\nINFO - Connection to 91.108.56.170:443/TcpFull complete!",
+            "telethon.network.mtprotosender -\nINFO - Disconnection from 91.108.56.146:443/TcpFull complete!",
+            "telethon.network.mtprotosender -\nINFO - Disconnecting from 91.108.56.146:443/TcpFull...",
+            "INFO - Modules to load: ['__help', 'admin', 'adzan', 'afk', 'allscrapers', 'android', 'anilist', 'animasi', 'anime', 'anti_spambot', 'antiflood', 'aria', 'ascii', 'bitly', 'blacklist', 'carbon', 'chat', 'coolprofilepics', 'covid', 'create', 'createstickers', 'dbs', 'deepfry', 'deezloader', 'detection', 'emojigames', 'eval', 'fakegban', 'federasi', 'figlet', 'filemanager', 'filter', 'games', 'gban', 'gcast', 'gdrive', 'get_user_id', 'getmusic', 'gid', 'gitcommit', 'github', 'glitcher', 'globalban', 'googlephotos', 'gps', 'hack', 'hash', 'help', 'hentai', 'herokuapp', 'igsaver', 'imgmemes', 'imp', 'kekuatan', 'kenzo', 'kenzogans', 'kenzogans24', 'lastfm', 'lock', 'lynxae', 'lynxhelper', 'lynxhz', 'lynxmemes', 'lynxmisc', 'lynxtt', 'lynxwc', 'lyrics', 'mega_downloads', 'memes', 'memify', 'mentions', 'messages', 'misc', 'nekobot', 'notes', 'offline', 'oi', 'phreaker', 'pms', 'profile', 'quotly', 'rastick', 'resi', 'reverse', 'salam', 'sangmata', 'santet', 'sed', 'snips', 'spam', 'spotifynow', 'ss_video', 'statme', 'stext', 'stickers', 'stickers_v2', 'system_stats', 'tag_all', 'telegraph', 'tempmail', 'time_date', 'tiny', 'torrentsearch', 'transform', 'updater', 'upload_download', 'waifu','transform', 'updater', 'upload_download', 'waifu', 'wallpaper', 'weather', 'webupload', 'welcomes', 'whois', 'www', 'xiaomi', 'zipfile']", 
+            "telethon.network.mtprotosender -\nINFO - Connecting to 91.108.56.146:443/TcpFull...",
+            "telethon.network.mtprotosender -\nINFO - Connection to 91.108.56.146:443/TcpFull complete!",
+            "telethon.network.mtprotosender -\nINFO - Received response without parent request",
+            "INFO - Lynx-Userbot:Logged in as 557667062`",
+            "INFO - Lynx-Userbot:Successfully loaded all plugins",
+            "919852+00:00 app[worker.1]: 919 - Lynx-Userbot -",
             "INFO - ⚡𝗟𝘆𝗻𝘅-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡ ⚙️ V4.0 [TELAH DIAKTIFKAN!]",
             "**Build Succeeded**"
 
@@ -76,7 +65,7 @@ async def _(event):
 
             await asyncio.sleep(animation_interval)
 
-            await event.edit(animation_chars[i % 12])
+            await event.edit(animation_chars[i % 22])
 
 
 CMD_HELP.update({
