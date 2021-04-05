a8fdfd5e3a3a8335b3c0fbab3ba1e476dd33993f
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-04-05 22:31:21 +0000

    Create deploy.py

--- userbot/modules/deploy.py
+++ userbot/modules/deploy.py
@@ -17,8 +17,8 @@ from platform import uname
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
-@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-async def _(event):
+@register(outgoing=True, pattern="^.deploy ?(.*)")
+async def deploys(event):
 
     if event.fwd_from:
 
