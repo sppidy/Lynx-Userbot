3d1b124004adf88144b00fe5b9f61f1180c4fd6a
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-04-07 17:14:22 +0000

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -81,10 +81,10 @@ BOTLOG = sb(os.environ.get("BOTLOG", "True"))
 LOGSPAMMER = sb(os.environ.get("LOGSPAMMER", "False"))
 
 # Custom Module
-CUSTOM_PMPERMIT_TEXT = os.environ.get("CUSTOM_PMPERMIT", None)
+CUSTOM_PMPERMIT_TEXT = os.environ.get("CUSTOM_PMPERMIT_TEXT", None)
 
 # Pm Permit Img
-PM_PERMIT_PIC = os.environ.get("PM_PERMIT_PIC", None)
+PM_PERMIT_PIC = os.environ.get("PM_PERMIT_PIC", None) or "https://telegra.ph/file/49ce66ba7e0fa0ce99210.png"
 
 # Bleep Blop, this is a bot ;)
 PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
