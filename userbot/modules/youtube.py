109d3e97b9564722feb2dd07372b8236e9ea3e3f
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-04-08 01:05:30 +0000

    Update youtube.py

--- userbot/modules/youtube.py
+++ userbot/modules/youtube.py
@@ -39,146 +39,111 @@ from userbot.utils import progress
 
 
 @register(outgoing=True, pattern=r"\.(ytaud|ytvid) (.*)")
-async def download_video(v_url):
-    """ For .ytdl command, download media from YouTube and many other sites. """
-    url = v_url.pattern_match.group(2)
-    if not url:
-        rmsg = await v_url.get_reply_message()
-        myString = rmsg.text
-        url = re.search("(?P<url>https?://[^\s]+)", myString).group("url")
-    if not url:
-        await edit_or_reply(v_url, "What I am Supposed to find? Give link")
-        return
-    ytype = v_url.pattern_match.group(1).lower()
-    v_url = await edit_or_reply(v_url, "`Preparing to download...`")
-    reply_to_id = await reply_id(v_url)
-    if ytype == "ytaud":
-        opts = {
-            "format": "bestaudio",
-            "addmetadata": True,
-            "key": "FFmpegMetadata",
-            "writethumbnail": True,
-            "prefer_ffmpeg": True,
-            "geo_bypass": True,
-            "nocheckcertificate": True,
-            "postprocessors": [
-                {
-                    "key": "FFmpegExtractAudio",
-                    "preferredcodec": "mp3",
-                    "preferredquality": "320",
-                }
-            ],
-            "outtmpl": "%(id)s.mp3",
-            "quiet": True,
-            "logtostderr": False,
-        }
-        video = False
-        song = True
-    elif ytype == "ytvid":
-        opts = {
-            "format": "best",
-            "addmetadata": True,
-            "key": "FFmpegMetadata",
-            "writethumbnail": True,
-            "prefer_ffmpeg": True,
-            "geo_bypass": True,
-            "nocheckcertificate": True,
-            "postprocessors": [
-                {"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}
-            ],
-            "outtmpl": "%(id)s.mp4",
-            "logtostderr": False,
-            "quiet": True,
-        }
-        song = False
-        video = True
-    try:
-        await v_url.edit("`Fetching data, please wait..`")
-        with YoutubeDL(opts) as ytdl:
-            ytdl_data = ytdl.extract_info(url)
-    except DownloadError as DE:
-        await v_url.edit(f"`{str(DE)}`")
-        return
-    except ContentTooShortError:
-        await v_url.edit("`The download content was too short.`")
-        return
-    except GeoRestrictedError:
-        await v_url.edit(
-            "`Video is not available from your geographic location due to geographic restrictions imposed by a website.`"
-        )
-        return
-    except MaxDownloadsReached:
-        await v_url.edit("`Max-downloads limit has been reached.`")
-        return
-    except PostProcessingError:
-        await v_url.edit("`There was an error during post processing.`")
-        return
-    except UnavailableVideoError:
-        await v_url.edit("`Media is not available in the requested format.`")
-        return
-    except XAttrMetadataError as XAME:
-        await v_url.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
-        return
-    except ExtractorError:
-        await v_url.edit("`There was an error during info extraction.`")
-        return
-    except Exception as e:
-        await v_url.edit(f"{str(type(e)): {str(e)}}")
-        return
-    c_time = time.time()
-    catthumb = Path(f"{ytdl_data['id']}.jpg")
-    if not os.path.exists(catthumb):
-        catthumb = Path(f"{ytdl_data['id']}.webp")
-    if not os.path.exists(catthumb):
-        catthumb = None
-    if song:
-        await v_url.edit(
-            f"`Preparing to upload song:`\
-        \n**{ytdl_data['title']}**\
-        \nby *{ytdl_data['uploader']}*"
-        )
-        await v_url.client.send_file(
-            v_url.chat_id,
-            f"{ytdl_data['id']}.mp3",
-            supports_streaming=True,
-            thumb=catthumb,
-            reply_to=reply_to_id,
-            attributes=[
-                DocumentAttributeAudio(
-                    duration=int(ytdl_data["duration"]),
-                    title=str(ytdl_data["title"]),
-                    performer=str(ytdl_data["uploader"]),
-                )
-            ],
-            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                progress(
-                    d, t, v_url, c_time, "Uploading..", f"{ytdl_data['title']}.mp3"
-                )
-            ),
-        )
-        os.remove(f"{ytdl_data['id']}.mp3")
-    elif video:
-        await v_url.edit(
-            f"`Preparing to upload video:`\
-        \n**{ytdl_data['title']}**\
-        \nby *{ytdl_data['uploader']}*"
-        )
-        await v_url.client.send_file(
-            v_url.chat_id,
-            f"{ytdl_data['id']}.mp4",
-            reply_to=reply_to_id,
-            supports_streaming=True,
-            caption=ytdl_data["title"],
-            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                progress(
-                    d, t, v_url, c_time, "Uploading..", f"{ytdl_data['title']}.mp4"
-                )
-            ),
-        )
-        os.remove(f"{ytdl_data['id']}.mp4")
-    if catthumb:
-        os.remove(catthumb)
-    await v_url.delete()
+ async def download_video(v_url):
+     url = v_url.pattern_match.group(2)
+     type = v_url.pattern_match.group(1).lower()
+
+     await v_url.edit("`Preparing to download...`")
+
+     if type == "ytaud":
+         opts = {
+             "format": "bestaudio",
+             "addmetadata": True,
+             "key": "FFmpegMetadata",
+             "writethumbnail": True,
+             "prefer_ffmpeg": True,
+             "geo_bypass": True,
+             "nocheckcertificate": True,
+             "postprocessors": [
+                 {
+                     "key": "FFmpegExtractAudio",
+                     "preferredcodec": "mp3",
+                     "preferredquality": "320",
+                 }
+             ],
+             "outtmpl": "%(id)s.mp3",
+             "quiet": True,
+             "logtostderr": False,
+         }
+         video = False
+         song = True
+
+     elif type == "ytvid":
+         opts = {
+             "format": "best",
+             "addmetadata": True,
+             "key": "FFmpegMetadata",
+             "prefer_ffmpeg": True,
+             "geo_bypass": True,
+             "nocheckcertificate": True,
+             "postprocessors": [
+                 {"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}
+             ],
+             "outtmpl": "%(id)s.mp4",
+             "logtostderr": False,
+             "quiet": True,
+         }
+         song = False
+         video = True
+
+     try:
+         await v_url.edit("`Fetching data, please wait..`")
+         with YoutubeDL(opts) as rip:
+             rip_data = rip.extract_info(url)
+     except DownloadError as DE:
+         return await v_url.edit(f"`{str(DE)}`")
+     except ContentTooShortError:
+         return await v_url.edit("`The download content was too short.`")
+     except GeoRestrictedError:
+         return await v_url.edit(
+             "`Video is not available from your geographic location "
+             "due to geographic restrictions imposed by a website.`"
+         )
+     except MaxDownloadsReached:
+         return await v_url.edit("`Max-downloads limit has been reached.`")
+     except PostProcessingError:
+         return await v_url.edit("`There was an error during post processing.`")
+     except UnavailableVideoError:
+         return await v_url.edit("`Media is not available in the requested format.`")
+     except XAttrMetadataError as XAME:
+         return await v_url.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
+     except ExtractorError:
+         return await v_url.edit("`There was an error during info extraction.`")
+     except Exception as e:
+         return await v_url.edit(f"{str(type(e)): {str(e)}}")
+     c_time = time.time()
+     if song:
+         await v_url.edit(f"`Preparing to upload song:`\n**{rip_data['title']}**")
+         await v_url.client.send_file(
+             v_url.chat_id,
+             f"{rip_data['id']}.mp3",
+             supports_streaming=True,
+             attributes=[
+                 DocumentAttributeAudio(
+                     duration=int(rip_data["duration"]),
+                     title=str(rip_data["title"]),
+                     performer=str(rip_data["uploader"]),
+                 )
+             ],
+             progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                 progress(d, t, v_url, c_time, "Uploading..", f"{rip_data['title']}.mp3")
+             ),
+         )
+         os.remove(f"{rip_data['id']}.mp3")
+         await v_url.delete()
+     elif video:
+         await v_url.edit(f"`Preparing to upload video:`\n**{rip_data['title']}**")
+         await v_url.client.send_file(
+             v_url.chat_id,
+             f"{rip_data['id']}.mp4",
+             supports_streaming=True,
+             caption=rip_data["title"],
+             progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                 progress(d, t, v_url, c_time, "Uploading..", f"{rip_data['title']}.mp4")
+             ),
+         )
+         os.remove(f"{rip_data['id']}.mp4")
+         await v_url.delete()
 
 
 @register(outgoing=True, pattern=r"^\.ytsearch (.*)")
