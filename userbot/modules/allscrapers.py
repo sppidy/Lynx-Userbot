b6d411cb002fa95e071b8a3b9c663a74b673e0bd
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-04-08 00:28:55 +0000

    Update allscrapers.py

--- userbot/modules/allscrapers.py
+++ userbot/modules/allscrapers.py
@@ -537,132 +537,6 @@ async def lang(value):
         )
 
 
-@register(outgoing=True, pattern=r"^\.yt (.*)")
-async def yt_search(video_q):
-    query = video_q.pattern_match.group(1)
-    if not query:
-        await video_q.edit("`Enter query to search`")
-    await video_q.edit("`Processing...`")
-    try:
-        results = json.loads(YoutubeSearch(query, max_results=7).to_json())
-    except KeyError:
-        return await video_q.edit(
-            "`Youtube Search gone retard.\nCan't search this query!`"
-        )
-    output = f"**Search Query:**\n`{query}`\n\n**Results:**\n\n"
-    for i in results["videos"]:
-        output += f"● `{i['title']}`\nhttps://www.youtube.com{i['url_suffix']}\n\n"
-    await video_q.edit(output, link_preview=False)
-
-
-@register(outgoing=True, pattern=r"\.(aud|vid) (.*)")
-async def download_video(v_url):
-    url = v_url.pattern_match.group(2)
-    type = v_url.pattern_match.group(1).lower()
-
-    await v_url.edit("`Preparing to download...`")
-
-    if type == "aud":
-        opts = {
-            "format": "bestaudio",
-            "addmetadata": True,
-            "key": "FFmpegMetadata",
-            "writethumbnail": True,
-            "prefer_ffmpeg": True,
-            "geo_bypass": True,
-            "nocheckcertificate": True,
-            "postprocessors": [
-                {
-                    "key": "FFmpegExtractAudio",
-                    "preferredcodec": "mp3",
-                    "preferredquality": "320",
-                }
-            ],
-            "outtmpl": "%(id)s.mp3",
-            "quiet": True,
-            "logtostderr": False,
-        }
-        video = False
-        song = True
-
-    elif type == "vid":
-        opts = {
-            "format": "best",
-            "addmetadata": True,
-            "key": "FFmpegMetadata",
-            "prefer_ffmpeg": True,
-            "geo_bypass": True,
-            "nocheckcertificate": True,
-            "postprocessors": [
-                {"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}
-            ],
-            "outtmpl": "%(id)s.mp4",
-            "logtostderr": False,
-            "quiet": True,
-        }
-        song = False
-        video = True
-
-    try:
-        await v_url.edit("`Fetching data, please wait..`")
-        with YoutubeDL(opts) as rip:
-            rip_data = rip.extract_info(url)
-    except DownloadError as DE:
-        return await v_url.edit(f"`{str(DE)}`")
-    except ContentTooShortError:
-        return await v_url.edit("`The download content was too short.`")
-    except GeoRestrictedError:
-        return await v_url.edit(
-            "`Video is not available from your geographic location "
-            "due to geographic restrictions imposed by a website.`"
-        )
-    except MaxDownloadsReached:
-        return await v_url.edit("`Max-downloads limit has been reached.`")
-    except PostProcessingError:
-        return await v_url.edit("`There was an error during post processing.`")
-    except UnavailableVideoError:
-        return await v_url.edit("`Media is not available in the requested format.`")
-    except XAttrMetadataError as XAME:
-        return await v_url.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
-    except ExtractorError:
-        return await v_url.edit("`There was an error during info extraction.`")
-    except Exception as e:
-        return await v_url.edit(f"{str(type(e)): {str(e)}}")
-    c_time = time.time()
-    if song:
-        await v_url.edit(f"`Preparing to upload song:`\n**{rip_data['title']}**")
-        await v_url.client.send_file(
-            v_url.chat_id,
-            f"{rip_data['id']}.mp3",
-            supports_streaming=True,
-            attributes=[
-                DocumentAttributeAudio(
-                    duration=int(rip_data["duration"]),
-                    title=str(rip_data["title"]),
-                    performer=str(rip_data["uploader"]),
-                )
-            ],
-            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                progress(d, t, v_url, c_time, "Uploading..", f"{rip_data['title']}.mp3")
-            ),
-        )
-        os.remove(f"{rip_data['id']}.mp3")
-        await v_url.delete()
-    elif video:
-        await v_url.edit(f"`Preparing to upload video:`\n**{rip_data['title']}**")
-        await v_url.client.send_file(
-            v_url.chat_id,
-            f"{rip_data['id']}.mp4",
-            supports_streaming=True,
-            caption=rip_data["title"],
-            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                progress(d, t, v_url, c_time, "Uploading..", f"{rip_data['title']}.mp4")
-            ),
-        )
-        os.remove(f"{rip_data['id']}.mp4")
-        await v_url.delete()
-
-
 def deEmojify(inputString):
     return get_emoji_regexp().sub("", inputString)
 
@@ -1446,11 +1320,7 @@ CMD_HELP.update(
         "trt": "⚡𝘾𝙈𝘿⚡: `.trt <text> [or reply]`"
         "\n↳ : Translates text to the language which is set."
         "\nUse >`.lang trt <language code>` to set language for trt. (Default is English)",
-        "youtube": "⚡𝘾𝙈𝘿⚡: `.yt <text>`" "\nUsage: Does a YouTube search.",
         "imdb": "⚡𝘾𝙈𝘿⚡: `.imdb <movie-name>`" "\n↳ : Shows movie info and other stuff.",
-        "rip": "⚡𝘾𝙈𝘿⚡: `.aud <url> or vid <url>`"
-        "\n↳ : Download videos and songs from YouTube "
-        "(and [many other sites](https://ytdl-org.github.io/youtube-dl/supportedsites.html)).",
         "wolfram": "⚡𝘾𝙈𝘿⚡: `.wolfram` <query>"
         "\n↳ : Get answers to questions using WolframAlpha Spoken Results API.",
         "screenshot": "⚡𝘾𝙈𝘿⚡: `.ss <url>`"
