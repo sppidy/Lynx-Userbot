ad41366bda70a721ae8ad5989023e9ca7365491b
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-04-10 23:30:53 +0000

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -28,7 +28,7 @@ from PIL import Image
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 REPOLINK = str(UPSTREAM_REPO_URL) if UPSTREAM_REPO_URL else "https://github.com/KENZO-404/Lynx-Userbot"
- ============================================
+# ============================================
 
 opener = urllib.request.build_opener()
 useragent = 'Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.70 Mobile Safari/537.36'
