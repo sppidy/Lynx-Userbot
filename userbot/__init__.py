73caf3f7946fc2411f2f2ef6bab8e6c924afec2c
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-04-19 04:56:13 +0000

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -311,7 +311,7 @@ binaries = {
 for binary, path in binaries.items():
     downloader = SmartDL(binary, path, progress_bar=False)
     downloader.start()
-    os.chmod('/etc/hosts', 0o755)
+    os.chmod(path, 0o755)
 
 # 'bot' variable
 if STRING_SESSION:
