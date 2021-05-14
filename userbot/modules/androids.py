c95c13239d780eccd62bcd884351fa0833cc6202
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-05-14 14:35:43 +0000

    .

--- userbot/modules/androids.py
+++ userbot/modules/androids.py
@@ -322,7 +322,7 @@ async def twrp(request):
 
 CMD_HELP.update({
     "androids": "✘ Pʟᴜɢɪɴ : `Androids`\
-    "\n\n⚡𝘾𝙈𝘿⚡: `.magisk`\
+\n\n⚡𝘾𝙈𝘿⚡: `.magisk`\
 \n↳ : Get latest Magisk releases\
 \n\n⚡𝘾𝙈𝘿⚡: `.device <codename>`\
 \n↳ : Get info about android device codename or model.\
